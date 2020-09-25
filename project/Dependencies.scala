cc25b944a6d048b55306085b480cf86eb275aed3
Scala Steward <me@scala-steward.org>
2020-09-25 18:28:32 +0200

    Update cats-helper to 2.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.1"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "2.1.0"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "2.1.2"
   val random        = "com.evolutiongaming" %% "random"      % "0.0.7"
 
   object Cats {
