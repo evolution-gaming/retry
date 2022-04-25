2b4417218099948ffe9af9acf898a5ad3c1ea496
Scala Steward <me@scala-steward.org>
2022-04-25 15:39:41 +0200

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.9"
+  val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.12"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.0.1"
   val random        = "com.evolutiongaming" %% "random"      % "1.0.0"
   val `cats-effect` = "org.typelevel"       %% "cats-effect" % "3.3.4"
