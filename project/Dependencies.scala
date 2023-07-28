8ac7245f29dbdcdf3795d7c82801988b9a0b06a9
Scala Steward <scala_steward@virtuslab.com>
2023-07-28 16:34:12 +0000

    Revert commit(s) 39ec188

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.9"
-  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.0.3"
+  val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.0.1"
   val random        = "com.evolutiongaming" %% "random"      % "1.0.0"
   val `cats-effect` = "org.typelevel"       %% "cats-effect" % "3.3.4"
 }
