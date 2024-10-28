f1af51f97f59b7fca7efc49825e87c91247f6dca
Scala Steward <scala_steward@virtuslab.com>
2024-10-28 16:12:36 +0000

    Update cats-effect to 3.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   val scalatest     = "org.scalatest"       %% "scalatest"   % "3.2.18"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.10.3"
   val random        = "com.evolution"       %% "random"      % "1.0.4"
-  val `cats-effect` = "org.typelevel"       %% "cats-effect" % "3.4.11"
+  val `cats-effect` = "org.typelevel"       %% "cats-effect" % "3.5.5"
 }
